8c79526e1bf11b0cfd293132a834fbce6d44ca79
DinoChiesa <dchiesa@google.com>
2025-05-19 17:07:02 +0000

    add brun.sh helper script

---
+++ brun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# -*- mode:shell-script; coding:utf-8; -*-
+
+MAVEN_OPTS="--enable-native-access=ALL-UNNAMED" java --enable-native-access=ALL-UNNAMED -jar backend/target/devportal-exp-backend-20250411.jar
